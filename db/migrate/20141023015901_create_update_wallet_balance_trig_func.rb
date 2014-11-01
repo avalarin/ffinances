17c926d5fba55f55436478440ff1016c99cb0145
avalarin <avalarin@gmail.com>
2014-11-01 18:33:00 +0600

    Исправлены отступы

--- db/migrate/20141023015901_create_update_wallet_balance_trig_func.rb
+++ db/migrate/20141023015901_create_update_wallet_balance_trig_func.rb
@@ -1,23 +1,23 @@
 class CreateUpdateWalletBalanceTrigFunc < ActiveRecord::Migration
   def up
     execute <<-SQL
-      CREATE OR REPLACE FUNCTION update_wallet_balance_trig() 
+      CREATE OR REPLACE FUNCTION update_wallet_balance_trig()
         RETURNS TRIGGER
       AS
-      $BODY$    
+      $BODY$
         BEGIN
           IF (TG_OP = 'INSERT') THEN
-              UPDATE wallets 
+              UPDATE wallets
                 SET balance = balance + NEW.sum * NEW.currency_rate
                 WHERE wallets.id = NEW.wallet_id;
               RETURN NEW;
           ELSIF (TG_OP = 'UPDATE') THEN
-              UPDATE wallets 
+              UPDATE wallets
                 SET balance = balance - OLD.sum * OLD.currency_rate + NEW.sum * NEW.currency_rate
                 WHERE wallets.id = NEW.wallet_id;
               RETURN NEW;
           ELSIF (TG_OP = 'DELETE') THEN
-            UPDATE wallets 
+            UPDATE wallets
                 SET balance = balance - OLD.sum * OLD.currency_rate
                 WHERE wallets.id = OLD.wallet_id;
               RETURN OLD;
