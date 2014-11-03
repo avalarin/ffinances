02d0f9fc1695149f6db249567c1c56d1b1dc057c
avalarin <avalarin@gmail.com>
2014-11-03 08:18:35 +0600

    Номер ревизии по возможности получается из файла REVISION

--- config/initializers/02_app.rb
+++ config/initializers/02_app.rb
@@ -1,6 +1,11 @@
 module FamilyFinances
   VERSION = File.read(Rails.root.join("VERSION")).strip
-  REVISION = (`git show --pretty=%H`[0..8]).chomp
+
+  if File.exist? Rails.root.join("REVISION")
+    REVISION = File.read(Rails.root.join("REVISION")).strip
+  else
+    REVISION = (`git show --pretty=%H`[0..8]).chomp
+  end
 end
 
 Rails.application.routes.default_url_options[:host] = Settings.host_name
