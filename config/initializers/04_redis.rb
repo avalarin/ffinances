a678c0a997e93ddc38b7bc809f4078b313196eef
avalarin <avalarin@gmail.com>
2014-11-04 22:32:28 +0600

    Исправлена ошибка из-за которой не удавалось подулючиться к redis по ip

--- config/initializers/04_redis.rb
+++ config/initializers/04_redis.rb
@@ -1,8 +1,8 @@
 require "redis"
 
 class Redis
-  uri = URI.parse(Settings.redis)
-  @@instance = Redis.new(path: uri.path ,host: uri.host, port: uri.port, password: uri.password, thread_safe: true)
+  @@instance = Redis.new(url: Settings.redis, thread_safe: true)
+
   def self.instance
     @@instance
   end
