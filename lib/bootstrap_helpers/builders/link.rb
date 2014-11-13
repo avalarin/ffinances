e7a928b2b0f34702b6401a093ceea05c694c5ba1
avalarin <avalarin@gmail.com>
2014-11-14 00:43:12 +0600

    Для ссылок по умолчанию устанавливается href=javascript: + рефакторинг

--- lib/bootstrap_helpers/builders/link.rb
+++ lib/bootstrap_helpers/builders/link.rb
@@ -7,45 +7,45 @@ module BootstrapHelpers
 
         content = options[:text] || capture_content
 
-        if (options[:icon])
+        if options[:icon]
           icon_html = template.capture do
             template.bt_icon options[:icon]
           end
-          content = icon_html + " " + content
+          content = icon_html + ' ' + content
         end
 
         css = Link.get_link_class options
         html = get_html_attributes css, options, {
           id: options[:id],
           title: options[:title],
-          href: options[:href]
+          href: options[:href] || 'javascript:'
         }
         html[:title] ||= options[:text]
         
         template.content_tag :a, content, html
       end
 
-      def self.get_link_class options
-        css = "link"
-        css << " " << Link.get_link_style_class(options[:style])
-        css << " " << Link.get_link_size_class(options[:size])
+      def self.get_link_class(options)
+        css = 'link'
+        css << ' ' << Link.get_link_style_class(options[:style])
+        css << ' ' << Link.get_link_size_class(options[:size])
         css
       end
 
-      def self.get_link_style_class style
+      def self.get_link_style_class(style)
         "link-#{ style }"
       end
 
-      def self.get_link_size_class size
+      def self.get_link_size_class(size)
         case size
           when :large
-            "link-lg"
+            'link-lg'
           when :small
-            "link-sm"
+            'link-sm'
           when :xsmall
-            "link-xs"
+            'link-xs'
           else
-            ""
+            ''
         end
       end
 
