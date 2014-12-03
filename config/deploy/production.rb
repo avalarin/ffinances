7d78d1578b503d43af04b99d0ecf0e0a56775540
avalarin <avalarin@gmail.com>
2014-12-03 13:47:28 +0600

    Поддержка travis

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 set :deploy_to, '/var/www/ffinances'
+set :ssh_options, keys: ["config/deploy_id_rsa"] if File.exist?("config/deploy_id_rsa")
 
 server 'ffinances.avalarin.net',
   user: 'ffinances',
