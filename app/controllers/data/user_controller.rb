6f05df8ad84648b70ef9af8fbfcd5b7abfa36e6f
avalarin <avalarin@gmail.com>
2014-12-05 12:18:33 +0600

    Поиск пользователя без учета регистра

--- app/controllers/data/user_controller.rb
+++ app/controllers/data/user_controller.rb
@@ -1,10 +1,10 @@
 class Data::UserController < ApplicationController
-  
+
   def index
     @users = User.all
     if (params[:search])
-      s = "%#{params[:search]}%"
-      @users = @users.where("name like ? or email like ? or display_name like ?", s, s, s)  
+      s = "%#{ params[:search].mb_chars.downcase.to_s }%"
+      @users = @users.where("lower(name) like ? or lower(email) like ? or lower(display_name) like ?", s, s, s)
     end
     respond_to do |format|
       format.json do
