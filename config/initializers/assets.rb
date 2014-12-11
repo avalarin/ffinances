335c8cc5f8a2cdb78357cbd43a1f6f9d05e15c81
avalarin <avalarin@gmail.com>
2014-12-12 00:44:13 +0600

    css файлы подвергаются прекомпиляции

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,4 +3,4 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.assets.precompile += %w(views/*.js views/*.scss)
+Rails.application.config.assets.precompile += %w(views/*.js views/*.scss views/*.css)
