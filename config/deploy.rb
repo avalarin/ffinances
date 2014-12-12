bfe9adce14cca5454beb4736d65c93f0f60845a9
avalarin <avalarin@gmail.com>
2014-12-12 14:20:47 +0600

    Добавил в связанные директории public/assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,5 +9,5 @@ set :unicorn_pid, "#{shared_path}/run/unicorn.pid"
 set :rails_env, 'production'
 
 set :linked_files, %w{config/unicorn.rb config/database.yml config/settings.yml config/secrets.yml}
-set :linked_dirs, %w{public/system log}
+set :linked_dirs, %w{public/system public/assets log}
 set :log_level, :info
