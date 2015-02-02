fd0c4e89b60dde7649f166593744cb594be73b4b
avalarin <avalarin@gmail.com>
2015-02-02 21:18:49 +0600

    В списке кошельков суммы форматируются с учетом культуры

--- app/assets/javascripts/model/wallet.js
+++ app/assets/javascripts/model/wallet.js
@@ -4,6 +4,7 @@ function Wallet(data) {
   this.key = data.key
   this.displayName = data.display_name
   this.balance = data.balance || 0
+  this.balanceText = numeral(this.balance).format('0,0.00')
   this.imageUrl = data.image_url
   this.currency = new Currency(data.currency)
 }
