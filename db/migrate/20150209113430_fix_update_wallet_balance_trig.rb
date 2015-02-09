bb5a23ca5724a49a1a192d159a0ca0cf925e17cc
avalarin <avalarin@gmail.com>
2015-02-09 17:48:10 +0600

    Исправлена проблема при которой не обновлялся баланс кошелька при редактировании транз.-перевода с изменением кошельков

---
+++ db/migrate/20150209113430_fix_update_wallet_balance_trig.rb
@@ -0,0 +1,64 @@
+class FixUpdateWalletBalanceTrig < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+      CREATE OR REPLACE FUNCTION update_wallet_balance_trig()
+        RETURNS TRIGGER
+      AS
+      $BODY$
+        BEGIN
+          IF (TG_OP = 'INSERT') THEN
+              UPDATE wallets
+                SET balance = balance + NEW.sum * NEW.currency_rate
+                WHERE wallets.id = NEW.wallet_id;
+              RETURN NEW;
+          ELSIF (TG_OP = 'UPDATE') THEN
+              UPDATE wallets
+                SET balance = balance - OLD.sum * OLD.currency_rate
+                WHERE wallets.id = OLD.wallet_id;
+              UPDATE wallets
+                SET balance = balance + NEW.sum * NEW.currency_rate
+                WHERE wallets.id = NEW.wallet_id;
+              RETURN NEW;
+          ELSIF (TG_OP = 'DELETE') THEN
+            UPDATE wallets
+                SET balance = balance - OLD.sum * OLD.currency_rate
+                WHERE wallets.id = OLD.wallet_id;
+              RETURN OLD;
+          END IF;
+          RETURN NULL;
+        END;
+      $BODY$
+      LANGUAGE plpgsql VOLATILE;
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      CREATE OR REPLACE FUNCTION update_wallet_balance_trig()
+        RETURNS TRIGGER
+      AS
+      $BODY$
+        BEGIN
+          IF (TG_OP = 'INSERT') THEN
+              UPDATE wallets
+                SET balance = balance + NEW.sum * NEW.currency_rate
+                WHERE wallets.id = NEW.wallet_id;
+              RETURN NEW;
+          ELSIF (TG_OP = 'UPDATE') THEN
+              UPDATE wallets
+                SET balance = balance - OLD.sum * OLD.currency_rate + NEW.sum * NEW.currency_rate
+                WHERE wallets.id = NEW.wallet_id;
+              RETURN NEW;
+          ELSIF (TG_OP = 'DELETE') THEN
+            UPDATE wallets
+                SET balance = balance - OLD.sum * OLD.currency_rate
+                WHERE wallets.id = OLD.wallet_id;
+              RETURN OLD;
+          END IF;
+          RETURN NULL;
+        END;
+      $BODY$
+      LANGUAGE plpgsql VOLATILE;
+    SQL
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202181452) do
+ActiveRecord::Schema.define(version: 20150209113430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 20141202181452) do
     t.datetime "updated_at"
     t.string   "ip"
     t.string   "user_agent"
+    t.integer  "book_id"
   end
 
   add_index "sessions", ["key"], name: "index_sessions_on_key", unique: true, using: :btree
